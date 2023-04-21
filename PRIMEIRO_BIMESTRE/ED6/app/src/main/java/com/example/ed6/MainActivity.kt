41108dd83e662d3e0838471e26936a1fd118781b
Erick Krzyzanovski <erickkrzyz@gmail.com>
2023-04-20 23:19:37 -0300

    .

--- PRIMEIRO_BIMESTRE/ED6/app/src/main/java/com/example/ed6/MainActivity.kt
+++ PRIMEIRO_BIMESTRE/ED6/app/src/main/java/com/example/ed6/MainActivity.kt
@@ -1,60 +1,78 @@
 package com.example.ed6
 
-import android.content.Context
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.Toast
-import java.net.URLEncoder
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Tchau()
+        Ola()
+        AbrirImagem()
     }
 
-    // função para compartilhar uma mensagem via WhatsApp
-    fun shareMessageViaWhatsApp(message: String, context: Context) {
-        try {
-            // codifica a mensagem para que possa ser passada como parâmetro na URI
-            val encodedMessage = URLEncoder.encode(message, "UTF-8")
-            // cria a URI de compartilhamento com a mensagem como parâmetro
-            val uri = Uri.parse("whatsapp://send?text=$encodedMessage")
-            // cria um intent para abrir o WhatsApp e compartilhar a mensagem
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_TEXT, message)
-            intent.setPackage("com.whatsapp")
-            intent.data = uri
-            // verifica se o WhatsApp está instalado no dispositivo
-            if (intent.resolveActivity(context.packageManager) != null) {
-                // inicia a atividade de compartilhamento via WhatsApp
-                context.startActivity(intent)
-            } else {
-                // exibe uma mensagem de erro caso o WhatsApp não esteja instalado
-                Toast.makeText(context, "O WhatsApp não está instalado", Toast.LENGTH_SHORT).show()
-            }
-        } catch (e: Exception) {
-            // trata possíveis exceções durante a codificação da mensagem ou criação do intent
-            e.printStackTrace()
-            Toast.makeText(context, "Erro ao compartilhar mensagem", Toast.LENGTH_SHORT).show()
+
+    private fun Ola(){
+        val btn_Ola = findViewById<Button>(R.id.btn_Ola)
+
+
+        btn_Ola.setOnClickListener{
+            val Numero_Telefone = "+5541992818738"
+            val Menssagem = "Olá! Sou Android!"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://wa.me/$Numero_Telefone/?text=$Menssagem")
+            startActivity(intent)
         }
     }
+    private fun Tchau(){
+        val btn_Tchau = findViewById<Button>(R.id.btn_Tchau)
 
-    fun Buttons() {
-        val btnOla_click = findViewById<Button>(R.id.btnOla)
 
-        // define um ouvinte de eventos de clique para o botão
-        btnOla_click.setOnClickListener {
-            // adiciona aqui o código a ser executado quando o botão é clicado
-            shareMessageViaWhatsApp("Olá sou eu de novo", this)
-            // por exemplo, exibir uma mensagem de log
-            Log.d("MeuApp", "O botão foi clicado!")
+        btn_Tchau.setOnClickListener{
+            val Numero_Telefone = "+5541992818738"
+            val Menssagem = "Tchau!"
+            val intent = Intent(Intent.ACTION_VIEW)
+            val whatsappIntent = Intent(Intent.ACTION_SEND)
+            whatsappIntent.type = "image/*"
+            whatsappIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file:///sdcard/image.jpg"))
+            whatsappIntent.setPackage("com.whatsapp")
+            startActivity(whatsappIntent)
+            intent.data = Uri.parse("https://wa.me/$Numero_Telefone/?text=$Menssagem")
+            startActivity(intent)
         }
     }
 
 
+    companion object {
+        private const val SELECAO_GALERIA = 200
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SELECAO_GALERIA && resultCode == Activity.RESULT_OK && data != null) {
+            val uri = data.data
+            val imagem = findViewById<ImageView>(R.id.imageView)
+            imagem.setImageURI(uri)
+        }
+    }
+
+    private fun AbrirImagem(){
+        val btn_AbrirImagem = findViewById<Button>(R.id.btn_AbrirImagem)
+        btn_AbrirImagem.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult( intent, SELECAO_GALERIA)
+        }
+    }
 }
